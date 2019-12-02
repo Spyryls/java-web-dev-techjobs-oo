904c082110e5bbda46ced8cf97b900534afaa8d0
Spyryls <yr410ad@gmail.com>
2019-12-02 16:50:50 -0600

    Finished Part 5; Starting Part 6

--- src/Tests/JobTest.java
+++ src/Tests/JobTest.java
@@ -13,6 +13,7 @@ public class JobTest {
     Job test_job1;
     Job test_job2;
     Job test_job3;
+    Job test_job4;
 
     @Before
     public void createJobObjects() {
@@ -20,13 +21,12 @@ public class JobTest {
         test_job1 = new Job("Brewer Friend", new Employer("Frank n' Steins"), new Location("St. Louis"), new PositionType("Kitchen"), new CoreCompetency("Salty Attitude"));
         test_job2 = new Job("Painter Guy", new Employer("Paint Ninja Painting"), new Location("St. Louis"), new PositionType("Muralist"), new CoreCompetency("Hiding EasterEggs"));
         test_job3 = new Job("Painter Guy", new Employer("Paint Ninja Painting"), new Location("St. Louis"), new PositionType("Muralist"), new CoreCompetency("Hiding EasterEggs"));
+        test_job4 = new Job("A Good, Good Dog", new Employer(""), new Location("St. Louis"), new PositionType("Animal Companion"), new CoreCompetency("Being a gud, widdle, wovey, boogie-boo!"));
     }
 
     @Test // test that ids are different
     public void testSettingJobId() {
         assertTrue(test_job1.getId() != test_job2.getId());
-//        System.out.println(test_job1.getId());
-//        System.out.println(test_job2.getId());
     }
 
     @Test // test that each field is present
@@ -51,6 +51,11 @@ public class JobTest {
         assertTrue(pGuy.contains("Location: St. Louis\n"));
         assertTrue(pGuy.contains("Position Type: Muralist\n"));
         assertTrue(pGuy.contains("Core Competency: Hiding EasterEggs\n"));
+    }
+
+    @Test
+    public void testEmptyJobField() {
+        assertTrue(test_job4.toString().contains("Data Not Available"));
     }
 
 }
